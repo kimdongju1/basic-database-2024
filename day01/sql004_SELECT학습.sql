e3b366dba5a64b9a888d7881365035d4c92943c3
kimdongju1 <dkdlkdh@naver.com>
2024-03-29 09:02:37 +0900

    Update sql004_SELECT학습.sql

--- day01/sql004_SELECT학습.sql
+++ day01/sql004_SELECT학습.sql
@@ -14,3 +14,55 @@ SELECT *
 -- 실무에서는 속성, 컬럼명을 다 적는게 일반적
 SELECT bookid, bookname, publisher, price
   FROM Book;
+
+-- 도서에서 출판사를 검색하시오(중복제거)
+SELECT DISTINCT publisher
+  FROM Book;
+
+-- 조건검색(조건 연산자 사용)
+-- 가격이 20,000원 미만인 도서를 검색하시오.
+SELECT *
+  FROM Book
+ WHERE price < 20000;
+
+ -- 가격이 10,000이상 20,000원이하인 도서를 검색하시오
+ SELECT *
+  FROM Book
+ WHERE price > 10000 AND price <= 20000;
+
+ SELECT *
+   FROM Book
+  WHERE price BETWEEN 10000 AND 20000;
+
+  -- 출판사가 굿스포츠와 대한미디어인 도서를 검색하시오
+SELECT *
+  FROM Book
+ WHERE publisher IN ('굿스포츠', '대한미디어');
+
+  -- 출판사가 굿스포츠와 대한미디어가 아닌 도서를 검색하시오
+SELECT *
+  FROM Book
+ WHERE publisher NOT IN ('굿스포츠', '대한미디어');
+
+-- 축구의 역사를 출판한 출판사를 검색하시오
+SELECT bookname, publisher
+  FROM Book
+  WHERE bookname = '축구의 역사';
+
+-- 도서 이름에 축구가 포함된 출판사를 검색하시오
+SELECT bookname, publisher
+  FROM Book
+  WHERE bookname LIKE '축구%'; -- 축구라는 글자로 시작하는 
+
+SELECT bookname, publisher
+  FROM Book
+  WHERE bookname LIKE '%축구'; -- 축구라는 글자로 끝나는
+
+SELECT bookname, publisher
+  FROM Book
+  WHERE bookname LIKE '%축구%'; -- 축구라는 글자를 포함하는 모든 책
+
+-- 두글자에 구로 끝나는 단어로 시작되는 출판사를 검색하시오.
+SELECT bookname, publisher
+  FROM Book
+  WHERE bookname LIKE '_구%'; -- _(무슨글자든 한글자가 들어간) 구라는 글자로 시작하는 
